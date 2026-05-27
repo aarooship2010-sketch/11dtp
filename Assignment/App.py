27d409a319975636375d3aac3a9f2929122d3713
Aarooshi <aarooship.2010@gmail.com>
2026-05-28 08:53:21 +1200

    testing first sql function

--- Assignment/App.py
+++ Assignment/App.py
@@ -1,2 +1,9 @@
-print("Hello world!")
-print('I like coding')
+import sqlite3
+
+db = sqlite3.connect("Grades_tracker.db")
+cursor = db.cursor()
+sql= "SELECT * from Grades_tracker;"
+results = cursor.fetchall()
+print(results)
+
+db.close()
