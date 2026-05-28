08f337d68f361d538a68c3037d7f2cf63cf30934
Aarooshi <aarooship.2010@gmail.com>
2026-05-28 19:17:48 +1200

    Final  rendetion for 1st function

--- Assignment/App.py
+++ Assignment/App.py
@@ -1,37 +1,39 @@
-#docstring -Aarooshi Pandit-Grades tracking database
-#import
+# docstring - Aarooshi Pandit - Grades tracking database
+# import
 import sqlite3
 
-
-#constants and variables
+# constants and variables
 DATABASE = "grades.db"
 
-
-
-#functions
+# functions
 def print_all_students():
     '''print all students nicely'''
-    db = sqlite3.connect("grades.db")     #  Connect to your database file
+    db = sqlite3.connect(DATABASE)     # Connected using your constant variable
     cursor = db.cursor()
-    sql = "SELECT * FROM Students;"     #selects all data from student table 
-    cursor.execute(sql)            #  Execute the query (this sends the command to the database)
-    results = cursor.fetchall()     # Fetch the results from the cursor and print them
-    #loop through all the results
-    for students in results:
-        print(f"student_id       Name   Gender Ethnicity                       speed              ")
-            print(f"{students[1]:<20} {students[2]:<20} {students[3]:<10}")
-    #loop finished here
-    print(results)
-    db.close()            # 5. Clean up and close the connection
+    
+    sql = "SELECT * FROM Students;"    # selects all data from student table 
+    cursor.execute(sql)                # Execute the query
+    results = cursor.fetchall()        # Fetch the results from the cursor
+    
+    print(f"\n{'ID':<10}{'Name':<20}{'Gender':<12}{'Ethnicity':<15}")
+    print("-" * 57)
+    
+    # loop through all the results
+    for student in results:
+        # student[0] = ID, student[1] = Name, student[2] = Gender, student[3] = Ethnicity
+        print(f"{student[0]:<10}{student[1]:<20}{student[2]:<12}{student[3]:<15}")
+    
+    # loop finished here
+    db.close()                         # Clean up and close the connection
 
-
-
-#main code
+# main code
 while True:
-     user_input = input("What would you like to do.\n1. Print all students\n2.Exit")
+    user_input = input("\nWhat would you like to do?\n1. Print all students\n2. Exit\nSelect an option (1-2): ").strip()
+    
     if user_input == "1":
         print_all_students()
     elif user_input == "2":
+        print("\nThank you for using the system. Goodbye!")
         break
     else:
-        print('That was not an option\n')
+        print(' That was not an option. Please try again.\n')
